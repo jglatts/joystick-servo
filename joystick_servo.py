7db8ffeb7a06421a172abc8f4eb189bc288ddec1
John Glatts <33476958+jglatts@users.noreply.github.com>
2018-10-03 22:31:10 -0700

    Update joystick_servo.py

--- joystick_servo.py
+++ joystick_servo.py
@@ -15,16 +15,16 @@ import signal
 import atexit
 from time import sleep
 
-btn = 15	# Define button pin
+btn = 15 # Define button pin
 
 
 def setup():
 	""" Setup Raspberry Pi """
 
-	ADC0832_tmp.setup()				# Setup ADC0832
-	GPIO.setmode(GPIO.BOARD)	# Numbers GPIOs by physical location
-	GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)	# Setup button pin as input an pull it up
-    	GPIO.setup(16, GPIO.OUT)	# Servo Output pin
+	ADC0832_tmp.setup() # Setup ADC0832
+	GPIO.setmode(GPIO.BOARD) # Numbers GPIOs by physical location
+	GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Setup button pin as input an pull it up
+    	GPIO.setup(16, GPIO.OUT) # Servo Output pin
 	global state
 	state = ['up', 'down', 'left', 'right']	
 	p = GPIO.PWM(16, 50) # Servo PWM pin
@@ -35,19 +35,19 @@ def getResult():
 	"""get joystick results """
 
 	if ADC0832_tmp.getResult1() == 0:
-		return 1	#up
+		return 1 # up
 	if ADC0832_tmp.getResult1() == 255:
-		return 2	#down
+		return 2 # down
 	if ADC0832_tmp.getResult() == 0:
-                servoguy()	#call servo myguy
+                servoguy()# call servo myguy
                 print('Servo Running!')
-		return 3	#left, and servo starts to run
+		return 3# left, and servo starts to run
 	if ADC0832_tmp.getResult() == 255:
                 servoguy()
                 print('Servo Running!')
-		return 4		#right, and servo starts to run
+		return 4 # right, and servo starts to run
 	if GPIO.input(btn) == 0:
-		print 'Button is pressed!'		# Button pressed
+		print 'Button is pressed!' # Button pressed
 		
 
 def servoguy():
@@ -71,13 +71,13 @@ def loop():
 			print state[tmp - 1]
 
 def destory():
-	GPIO.cleanup()		# Release resource
+	GPIO.cleanup()	# sk8 and destroy 
 
 	
-if __name__ == '__main__':	# Program starts from here
+if __name__ == '__main__': # Program starts from here
 	setup()
 	try:
 		loop()
-	except KeyboardInterrupt:  	# When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+	except KeyboardInterrupt: # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
 		destory()
 		print('You have cancelled the program!')
